6f797d01f4edaf060ee92526541b7d907b28ad76
aanriantoni333 <93020252+aanriantoni333@users.noreply.github.com>
2021-10-24 17:08:37 +0700

    Update Browsh.sh

--- Browsh.sh
+++ Browsh.sh
@@ -1,10 +1,3 @@
-sudo apt-get install python-selenium
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-i686.tar.bz2
-tar -jxvf phantomjs-2.1.1-linux-i686.tar.bz2 
-cd phantomjs-2.1.1-linux-i686/
-sudo mv bin/phantomjs /usr/local/bin/
-cd ..
-sudo apt-get install python-tornado
-wget http://ssb22.user.srcf.net/adjuster/adjuster.py
-python adjuster.py --PhantomJS --real_proxy
-http_proxy=http://localhost:28080 lynx http://site-that-needs-javascript.example.com
+wget https://github.com/browsh-org/browsh/releases/download/v1.6.4/browsh_1.6.4_linux_amd64.deb
+sudo dpkg -i browsh_1.6.4_linux_amd64.deb
+browsh --help
