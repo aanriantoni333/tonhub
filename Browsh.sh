4d773cb7ab1d604d2bae255a3239f56cba8eabd9
aanriantoni333 <93020252+aanriantoni333@users.noreply.github.com>
2021-10-24 14:44:52 +0700

    Update Browsh.sh

--- Browsh.sh
+++ Browsh.sh
@@ -1,3 +1,4 @@
 wget https://github.com/browsh-org/browsh/releases/download/v1.6.4/browsh_1.6.4_linux_amd64.deb
 sudo dpkg -i browsh_1.6.4_linux_amd64.deb
-browsh www.youtube.com
+browsh
+browsh -starup-url https://www.youtube.com
