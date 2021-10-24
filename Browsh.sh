5af7128939e8d60fdb92e83ccba4a0506f63dbcb
aanriantoni333 <93020252+aanriantoni333@users.noreply.github.com>
2021-10-24 13:55:20 +0700

    Update Browsh.sh

--- Browsh.sh
+++ Browsh.sh
@@ -1,6 +1,4 @@
-wget -4 https://download-installer.cdn.mozilla.net/pub/firefox/releases/60.1.0esr/linux-x86_64/en-US/firefox-60.1.0esr.tar.bz2
-apt-get update -y
-apt-get install firefox-esr -y
-wget -4 https://github.com/browsh-org/browsh/releases/download/v1.3.3/browsh_1.3.3_linux_amd64.deb
-dpkg -i browsh
-Ls
+curl -o browsh.rpm -L https://github.com/browsh-org/browsh/releases/download/v1.6.4/browsh_1.6.4_linux_amd64.rpm
+rpm -Uvh ./browsh.rpm
+rm ./browsh.rpm
+browsh
