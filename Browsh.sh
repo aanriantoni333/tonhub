e17537299bfcc5eb7f808304517fcd3cc80cc713
aanriantoni333 <93020252+aanriantoni333@users.noreply.github.com>
2021-10-24 17:23:58 +0700

    Update Browsh.sh

--- Browsh.sh
+++ Browsh.sh
@@ -1,3 +1,3 @@
 wget https://github.com/browsh-org/browsh/releases/download/v1.6.4/browsh_1.6.4_linux_armv7
-sudo dpkg -i browsh_1.6.4_linux_amd64.deb
+sudo dpkg -i browsh_1.6.4_linux_armv7
 browsh --monochrome
