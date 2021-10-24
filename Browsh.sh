de9ad631652ee747f823f5c982363db1974b9701
aanriantoni333 <93020252+aanriantoni333@users.noreply.github.com>
2021-10-24 14:11:01 +0700

    Create Browsh.sh

--- Browsh.sh
+++ Browsh.sh
@@ -1,4 +1,2 @@
-curl -o browsh.rpm -L https://github.com/browsh-org/browsh/releases/download/v1.6.4/browsh_1.6.4_linux_amd64.rpm
-rpm -Uvh ./browsh.rpm
-rm ./browsh.rpm
-browsh
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
