18ecd77a7bb81a6c43806239828459b16e75f4c5
aanriantoni333 <93020252+aanriantoni333@users.noreply.github.com>
2021-10-24 14:23:56 +0700

    Update Browsh.sh

--- Browsh.sh
+++ Browsh.sh
@@ -1,7 +1,5 @@
-wget -4 https://download-installer.cdn.mozilla.net/pub/firefox/releases/60.1.0esr/linux-x86_64/en-US/firefox-60.1.0esr.tar.bz2
-apt-get update
-apt-get install firefox-esr
-wget -4 https://github.com/browsh-org/browsh/releases/download/v1.3.3/browsh_1.3.3_linux_amd64.deb
-dpkg -i browsh
-sudo ln -s /home/rochmads/firefox/firefox /usr/local/bin/
-browsh -startup-url http://youtube.com
+wget https://github.com/browsh-org/browsh/releases/download/v1.6.4/browsh_1.6.4_linux_amd64.deb
+sudo apt install ./browsh_1.6.4_linux_amd64.deb
+browsh
+browsh www.youtube.com
+Ls
