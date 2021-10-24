52ada337d2bb11e611ad8252b1e4a342169ca60f
aanriantoni333 <93020252+aanriantoni333@users.noreply.github.com>
2021-10-24 13:51:11 +0700

    Update Bubuntu2.sh

--- Bubuntu2.sh
+++ Bubuntu2.sh
@@ -1,6 +1,4 @@
 wget https://github.com/browsh-org/browsh/releases/download/v1.6.4/browsh_1.6.4_linux_amd64.deb
 sudo apt install ./browsh_1.6.4_linux_amd64.deb
-chmod a+x browsh_1.6.4_linux_amd64
-./browsh_1.6.4_linux_amd64
 rm ./browsh_1.6.4_linux_amd64.deb
 browsh
