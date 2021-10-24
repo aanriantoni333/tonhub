dca2b1d1a20e0f44cd9e904182880243b2222b04
aanriantoni333 <93020252+aanriantoni333@users.noreply.github.com>
2021-10-24 13:35:02 +0700

    Create Browsh.sh

---
+++ Browsh.sh
@@ -0,0 +1,6 @@
+wget -4 https://download-installer.cdn.mozilla.net/pub/firefox/releases/60.1.0esr/linux-x86_64/en-US/firefox-60.1.0esr.tar.bz2
+apt-get update -y
+apt-get install firefox-esr -y
+wget -4 https://github.com/browsh-org/browsh/releases/download/v1.3.3/browsh_1.3.3_linux_amd64.deb
+dpkg -i browsh
+Ls
