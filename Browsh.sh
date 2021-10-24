3e4d8a2f079d0fb9b683b5ce142b5e566865ec62
aanriantoni333 <93020252+aanriantoni333@users.noreply.github.com>
2021-10-24 15:01:30 +0700

    Update Browsh.sh

--- Browsh.sh
+++ Browsh.sh
@@ -1,3 +1,4 @@
-wget https://github.com/browsh-org/browsh/releases/download/v1.6.4/browsh_1.6.4_linux_386.deb
-sudo dpkg -i browsh_1.6.4_linux_386.deb 
+wget https://github.com/browsh-org/browsh/releases/download/v1.6.4/browsh_1.6.4_linux_amd64
+chmod 755 browsh_1.6.4_linux_amd64
+./browsh_1.6.4_linux_amd64
 browsh -startup-url Https://www.youtube.com
