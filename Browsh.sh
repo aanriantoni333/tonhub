1d0c6d247ad253ebb7f9819ee1e6e2fcc20c1991
aanriantoni333 <93020252+aanriantoni333@users.noreply.github.com>
2021-10-24 14:26:47 +0700

    Update Browsh.sh

--- Browsh.sh
+++ Browsh.sh
@@ -1,5 +1,5 @@
 wget https://github.com/browsh-org/browsh/releases/download/v1.6.4/browsh_1.6.4_linux_amd64.deb
-sudo apt install ./browsh_1.6.4_linux_amd64.deb
+sudo apt install /home/runner/work/ritoniaan/ritoniaan/browsh_1.6.4_linux_amd64.deb
 browsh
 browsh www.youtube.com
 Ls
